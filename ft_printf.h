71c950c60f1a6977a6362741a54b866ef68c29a8
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2024-03-21 22:01:38 +0100

    Create ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,15 @@
+#ifndef FT_PRINTF_H
+
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+char 	*ft_strchr(const char *s, int c);
+size_t	ft_putchar(char c);
+size_t	ft_putstr(char *s);
+int		printf(char const *,);
+
+
+#endif
