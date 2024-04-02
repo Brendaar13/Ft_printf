fc8449c3c424334a9290e2c2529e2431e21184e8
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2024-04-02 18:43:38 +0200

    Update ft_base.c

--- ft_base.c
+++ ft_base.c
@@ -1,49 +1,47 @@
 #include "ft_printf.h"
 
-size_t type_base(size_t num, char *ptr, size_t len_base)
+size_t	type_base(size_t num, char *ptr, size_t len_base)
 {
-    char array[20];
-    size_t c_printed; 
-    size_t res;
-    size_t i;
+	char	array[20];
+	size_t	c_printed;
+	size_t	res;
+	size_t	i;
 
-    i = 0;
-    c_print = 0;
-      if (num == 0)
-      c_printed += ft_putchar(base[0]);
-      if (num != 0)
-      {
-        res = num % len_base;
-        array[i] = ptr[res];
-        num = num / base_len;
-        ++i;
-      }
-      while (i--)
-      c_printed += ft_putchar(array[i]);
-    return (c_printed);
-}
-
-size_t ft_base(size_t num, char *ptr, size_t base, char c_exit)
-{
-  size_t  c_printed;
-
-  c_printed = 0;
-  
-  //Casos especiales
-
-    if (num < 0  && c_exit == 'd' || c_exit == 'u' || c_exit == 'i')
-      {
-  		c_printed += ft_putchar('-');
-  		num = -num;
-  	  }
-  	else if (num == 0 && c_exit == 'p')
-  	{
-  		c_printed += ft_putstr("0x0");
-  		return (c_printed);
-  	}
-  	else if (c_exit == 'p')
-  		c_printed += ft_putstr("0x");
-  	  c_printed += type_base(num, ptr, len_base);
+	i = 0;
+	c_printed = 0;
+	if (num == 0)
+		c_printed += ft_putchar(ptr[0]);
+	if (num != 0)
+	{
+		res = num % len_base;
+		array[i] = ptr[res];
+		num = num / len_base;
+		++i;
+	}
+	while (i--)
+		c_printed += ft_putchar(array[i]);
 	return (c_printed);
 }
 
+size_t	ft_base(size_t num, char *ptr, size_t base, char c_exit)
+{
+	size_t	c_printed;
+
+	c_printed = 0;
+	if (num < 0 && (c_exit == 'd' || c_exit == 'u' || c_exit == 'i'))
+	{
+		c_printed += ft_putchar('-');
+		num = -num;
+	}
+	else if (num == 0 && (c_exit == 'p'))
+	{
+		c_printed += ft_putstr("0x0");
+		return (c_printed);
+	}
+	else if (c_exit == 'p')
+	{
+		c_printed += ft_putstr("0x");
+		c_printed += type_base(num, ptr, base);
+	}
+	return (c_printed);
+}
