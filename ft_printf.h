feb73c00aff204d334f3bf7b667f02cea38dc104
Brenda Rojas <138994387+Brendaar13@users.noreply.github.com>
2024-10-19 21:04:11 +0200

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,11 +6,12 @@
 # include <stddef.h>
 # include <stdarg.h>
 
-int	ft_putchar(char c);
-int	ft_putstr(char *s);
+int     ft_putchar(char c);
+int     ft_putstr(char *s);
 int     ft_putnbr(int n);
 int     ft_neputnbr(unsigned int n);
 int     hexa(unsigned long n, char *hex);
-int		ft_printf(char const *,);
+int		ft_put_ptr(int *ptr);
+int		ft_printf(char const *, ...);
 
 #endif
