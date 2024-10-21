6f2a8e287d7a3d9b290ac6c3d8fc94b68750c574
Brenda Rojas <138994387+Brendaar13@users.noreply.github.com>
2024-10-21 02:59:26 +0200

    Update ft_printf_utils.c

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -2,24 +2,24 @@
 
 int	ft_putchar(char c)
 {
-		write(1, &c, 1);
-		return (1);
+	write(1, &c, 1);
+	return (1);
 }
 
-int     ft_putstr(char *s)
+int	ft_putstr(char *s)
 {
-        int     i;
+	int	i;
 
-        if (s == NULL)
-        {
-                write(1, "(null)", 6);
-                return (6);
-        }
-        i = 0;
-        while (s[i] != '\0')
-        {
-                write(1, &s[i], 1);
-                i++;
-        }
-        return (i);
+	if (s == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+	return (i);
 }
